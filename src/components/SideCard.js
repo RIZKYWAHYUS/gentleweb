909f77dac8a43e3c14a3215317c8f349b3facf35
RIZKYWAHYUS <rizkywahyu9999@gmail.com>
2019-08-07 15:02:05 +0700

    Testing deploy pakai AWS Amplify

--- src/components/SideCard.js
+++ src/components/SideCard.js
@@ -54,7 +54,7 @@ render() {
                     <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">Glad Chinda {this.state.theposition }</CardTitle>
                     <CardSubtitle className="text-secondary mb-3 font-weight-light text-uppercase" style={{ fontSize: '0.8rem' }}>Web Developer, Lagos</CardSubtitle>
                     <CardText className="text-secondary mb-4" style={{ fontSize: '0.75rem' }}>Full-stack web developer learning new hacks one day at a time. Web technology enthusiast. Hacking stuffs @theflutterwave.</CardText>
-                    <Button color="success" className="font-weight-bold">Beli</Button>
+                    <Button color="success" className="font-weight-bold">Beli Barang Ini</Button>
                 </CardBody>
                 </Card>
                 
